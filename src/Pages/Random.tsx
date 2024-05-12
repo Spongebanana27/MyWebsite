c37e0f9a06b7a3e5438a32ca6c0a2b219ad8e7e8
spongebanana27 <nsevans2707@gmail.com>
2024-05-11 23:39:14 -0400

    Added max arenas

--- src/Pages/Random.tsx
+++ src/Pages/Random.tsx
@@ -1,10 +1,13 @@
 import "../Home.css";
 import { useNavigate } from "react-router-dom";
+import max from "../assets/max.png";
+
 function Random() {
   const navigate = useNavigate();
   return (
     <>
       <h1 className="titleCard">RANDOM</h1>
+      <img className="favoritesPicture" src={max}></img>
       <button
         className="homeButton"
         onClick={() => {
