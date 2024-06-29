dc9595cd8d0408488d5a8e35d24975d0ddc0354b
spongebanana27 <nsevans2707@gmail.com>
2024-06-29 17:56:38 -0400

    Bugfix

--- src/Pages/Random.tsx
+++ src/Pages/Random.tsx
@@ -2,6 +2,8 @@ import "../Home.css";
 import "./Random.css";
 import { useNavigate } from "react-router-dom";
 import Board from "../games/ticTacToe/TicTacToe";
+import Atom from "../assets/ImgProcessing/Atom.mp4";
+import SinWaves from "../assetsImgProcessing/SinWaves.mp4";
 
 function Random() {
   const navigate = useNavigate();
@@ -16,13 +18,10 @@ function Random() {
       <div className="projectCard">
         <h2 className="projectHeader">Shader Art - GLSL</h2>
         <video className="shaderArt" autoPlay loop muted>
-          <source
-            src="/src/assets/ImgProcessing/SinWaves.mp4"
-            type="video/mp4"
-          />
+          <source src={SinWaves} type="video/mp4" />
         </video>
         <video className="shaderArt" autoPlay loop muted>
-          <source src="/src/assets/ImgProcessing/Atom.mp4" type="video/mp4" />
+          <source src={Atom} type="video/mp4" />
         </video>
       </div>
       <button
