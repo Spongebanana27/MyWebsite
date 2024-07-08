bd8115e17312576b19ccfb68276b5c9aacc24554
spongebanana27 <nsevans2707@gmail.com>
2024-07-08 10:49:51 -0400

    Changed about

--- src/NewPages/About.tsx
+++ src/NewPages/About.tsx
@@ -26,8 +26,8 @@ function About() {
               I am a problem solver and a passionate learner. While my main
               areas of interest are in 3D graphics and full stack web
               developement, I love to learn new things, having self-taught
-              various topics such as neural networks, game design, and 2D
-              shaders. When I am not coding, I enjoy reading, playing guitar and
+              various topics such as neural networks, game design, and 2D shader
+              art. When I am not coding, I enjoy reading, playing guitar and
               rock climibing.
             </div>
           </div>
