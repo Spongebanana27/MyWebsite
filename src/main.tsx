773c921cc335fbe62a44f7354db9dcb1aae08528
spongebanana27 <nsevans2707@gmail.com>
2024-08-26 19:05:45 -0400

    Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Routing from "./Routing.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Routing/>
+    <Analytics />
+    <Routing />
   </React.StrictMode>
 );
